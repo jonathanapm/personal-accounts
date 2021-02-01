ce46b0f70f19bbf4830076276cc2d9df3920b00a
jonathanapm <378623#Japm930522>
2021-02-01 19:45:33 -0300

    Definindo classes e estrutura do projeto

---
+++ src/main/java/com/personalaccounts/model/Salary.java
@@ -0,0 +1,22 @@
+package com.personalaccounts.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Salary extends AccountFlow {
+
+    private LocalDateTime deliveryDate;
+
+    @Override
+    void deposit(AccountFlow accountFlow) {
+
+    }
+}
