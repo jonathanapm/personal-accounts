43198ca66c4b5a8dc54a793920d260ef5cf26bc6
jonathanapm <378623#Japm930522>
2021-02-01 19:14:02 -0300

    Aplicando dependencias do projeto

---
+++ src/main/java/com/personalaccounts/PersonalAccountsApplication.java
@@ -0,0 +1,13 @@
+package com.personalaccounts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalAccountsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonalAccountsApplication.class, args);
+	}
+
+}
